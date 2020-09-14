2405ccf0ba4f2be1882a8d9e2511b8c5cd4905a2
unfelix <shevko.evheniy@gmail.com>
2020-09-15 01:40:17 +0300

    slick slider for dektop

--- app/js/main.js
+++ app/js/main.js
@@ -1,28 +1,20 @@
-const slider = document.querySelector(".slides");
-const leftArrow = document.getElementsByClassName("slider-arrow prev")[0];
-leftArrow.addEventListener("click", slideLeft);
-
-function slideLeft(e) {
-  slider.scrollBy({ behavior: "smooth", left: -320 });
-  leftArrow.removeEventListener("click", slideLeft);
-  setTimeout(() => {
-    if (slider.scrollLeft != 0) {
-      leftArrow.addEventListener("click", slideLeft);
-    }
-  }, 500);
-}
-
-slider.scroll(2000, 0);
-
-const rightArrow = document.getElementsByClassName("slider-arrow next")[0];
-rightArrow.addEventListener("click", slideRight);
-function slideRight(e) {
-  slider.scrollBy({ behavior: "smooth", right: +320 });
-  rightArrow.removeEventListener("click", slideRight);
-  setTimeout(() => {
-    if (slider.scrollLeft != 1920) {
-      rightArrow.addEventListener("click", slideRight);
-    }
-  }, 500);
-}
-slider.scroll(0, 2000);
+$(document).ready(function () {
+  $(".slider").slick({
+    slidesToShow: 3,
+    speed: 1500,
+    easing: "ease",
+    infinite: true,
+    // autoplay: true,
+    // autoplaySpeed: 2500,
+    centerMode: true,
+    responsive: [
+      {
+        breapoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+    mobileFirst: true,
+  });
+});
