a704a3ed2439f8b3ef083f72b20c2250e2f9c1b1
unfelix <shevko.evheniy@gmail.com>
2020-09-14 21:50:02 +0300

    hand write slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,28 @@
-$(function(){
+const slider = document.querySelector(".slides");
+const leftArrow = document.getElementsByClassName("slider-arrow prev")[0];
+leftArrow.addEventListener("click", slideLeft);
 
+function slideLeft(e) {
+  slider.scrollBy({ behavior: "smooth", left: -320 });
+  leftArrow.removeEventListener("click", slideLeft);
+  setTimeout(() => {
+    if (slider.scrollLeft != 0) {
+      leftArrow.addEventListener("click", slideLeft);
+    }
+  }, 500);
+}
 
+slider.scroll(2000, 0);
 
-  
-});
+const rightArrow = document.getElementsByClassName("slider-arrow next")[0];
+rightArrow.addEventListener("click", slideRight);
+function slideRight(e) {
+  slider.scrollBy({ behavior: "smooth", right: +320 });
+  rightArrow.removeEventListener("click", slideRight);
+  setTimeout(() => {
+    if (slider.scrollLeft != 1920) {
+      rightArrow.addEventListener("click", slideRight);
+    }
+  }, 500);
+}
+slider.scroll(0, 2000);
